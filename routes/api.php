f01368946da9f53cae201c1ab21a647e51b42c00
Cristian6597 <cristianercolani6597@gmail.com>
2025-06-16 11:22:18 +0200

    Auth finita

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ClientProfileController;
+use App\Http\Controllers\Api\ExerciseController;
+use App\Http\Controllers\Api\TrainerController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Auth Routes with Sanctum
+|--------------------------------------------------------------------------
+*/
+
+
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', 'user');
+        Route::post('/logout', 'logout');
+        Route::post('/logout-all', 'logoutAll');
+        Route::get('/tokens', 'tokens');
+    });
+});
